25e9105760b3403661e3be81a7204e763e66862e
Rishabh Pandey <rishabh42pandey@gmail.com>
2020-07-02 22:55:19 +0530

    Added exception handling for arguments

--- flask_folder.py
+++ flask_folder.py
@@ -1,3 +1,9 @@
 import sys
 
-print(f"Argument passed : { sys.argv[1] }")
+# Checking for Exception in case no Arguments are provided 
+try:
+    project_name = sys.argv[1]
+except:
+    project_name = input("Enter the name of the Flask Project: ")
+
+print(f"\n {project_name}")
