bc4da4f839d76fa9f7b4173be2a099a976cf89d3
Rishabh Pandey <rishabh42pandey@gmail.com>
2020-07-03 00:18:32 +0530

    Added all the files and folder

--- flask_folder.py
+++ flask_folder.py
@@ -8,11 +8,54 @@ except IndexError:
     project_name = input("Enter the name of the Flask Project: ")
 
 # Now creating Main Project folder
-
 try:
     # Create target Directory
     os.mkdir(project_name)
-    print("Directory " , project_name ,  " Created ") 
 except FileExistsError:
     print("Directory " , project_name ,  " already exists")
+
+
+# Moving inside the Main Project Folder
+os.chdir(project_name)
+
+# Now creating Sub folder
+os.mkdir("app")
+os.mkdir("migrations")
+os.mkdir("tests")
+
+# Now creating the files
+os.system("pip3 freeze >> requirements.txt")
+os.system("touch config.py")
+os.system(f"touch {project_name}.py")
+
+# Now moving inside app
+os.chdir("app")
+
+os.mkdir("templates")
+os.mkdir("static")
+os.mkdir("main")
+
+# Now creating the files
+os.system("touch __init__.py")
+os.system("touch models.py")
+
+# Moving inside main 
+os.chdir("main")
+
+# Now creating the files
+os.system("touch __init__.py")
+os.system("touch errors.py")
+os.system("touch views.py")
+
+os.chdir("..")
+os.chdir("..")
+
+# Moving inside tests
+os.chdir("tests")
+
+# Now creating the files
+os.system("touch __init__.py")
+os.system("touch test*.py")
+
+print("Flask project Structure Ready")
 
